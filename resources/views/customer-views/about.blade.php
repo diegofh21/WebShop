9891a02831bd2689772a728bb0dc0f6875160c5b
diegofh21 <dfuenmayor76@gmail.com>
2021-05-11 11:59:41 -0400

    actualizacion sobre nosotros

--- resources/views/customer-views/about.blade.php
+++ resources/views/customer-views/about.blade.php
@@ -33,23 +33,74 @@
         </div>
       </div>
     </div>
+
+    {{-- QUIENES SOMOS, QUE HACEMOS Y POR QUÉ --}}
+    <div class="col-sm-12">
+      <div class="section-title">
+        <span>Quienes somos, que hacemos y por qué</span>
+      </div>
+    </div>
     <div class="row">
-      <div class="col-lg-4 col-md-4 col-sm-6">
+      <div class="col-sm-12">
         <div class="about__item">
           <h4>¿Quienes somos?</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, corrupti odit? Eum, quaerat laudantium. Rem, reprehenderit? Numquam, a nisi? Ipsum aperiam perspiciatis minima ducimus excepturi consequatur voluptatum molestiae illum rerum?</p>
+          <p>Kalon nace como una idea de negocio familiar en donde la idea principal es ofrecer la mejor relación calidad precio para que todas las personas tengan acceso a piezas de ropa de alta calidad y siempre siendo atendidos de la mejor manera posible.</p>
         </div>
       </div>
-      <div class="col-lg-4 col-md-4 col-sm-6">
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
         <div class="about__item">
           <h4>¿Que hacemos?</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, corrupti odit? Eum, quaerat laudantium. Rem, reprehenderit? Numquam, a nisi? Ipsum aperiam perspiciatis minima ducimus excepturi consequatur voluptatum molestiae illum rerum?</p>
+          <p>Nosotros en Kalon nos especializamos en importar ropa desde USA y Europa, con estilos acordes a las tendencias de la moda para traer a Venezuela todas esas marcas que quizás no están presentes del todo en nuestro país y así no quedarnos atrás.</p>
         </div>
       </div>
-      <div class="col-lg-4 col-md-4 col-sm-6">
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
         <div class="about__item">
-          <h4>Otra pregunta acá</h4>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, corrupti odit? Eum, quaerat laudantium. Rem, reprehenderit? Numquam, a nisi? Ipsum aperiam perspiciatis minima ducimus excepturi consequatur voluptatum molestiae illum rerum?</p>
+          <h4>¿Por qué?</h4>
+          <p>Vimos una oportunidad de negocio que a pesar de que hay mucha competencia sabemos que podemos resaltar con la idea que queremos desarrollar. Venezuela gracias a la situación que está viviendo actualmente, estamos cohibidos de muchas tendencias de moda que por una razón o otra no tocan Venezuela y nuestra misión es hacer que cualquier venezolano pueda vestirse con lo último de cada marca como en cualquier país, facilitando todos los procesos de importación ya que recibirán su artículo a la puerta de su casa.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="dropdown-divider pb-4"></div>
+
+    {{-- MISION, VISION Y OBJETIVOS --}}
+    <div class="col-sm-12">
+      <div class="section-title">
+        <span>Misión, visión y objetivos</span>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="about__item">
+          <h4>Misión</h4>
+          <p>La misión de Kalon es ofrecer el mejor servicio posible, sabemos que con una gran atención la experiencia de compra se lleva a otro nivel y queremos que nuestros clientes salgan con una sonrisa en la cara para así crear esa lealtad con la marca y poder ir creciendo poco a poco </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="about__item">
+          <h4>Visión</h4>
+          <p>Nuestra visión de Kalon es llegar a tener un gran catálogo de marcas que nos permitan ser la opción principal de cualquier persona a la hora de hacer una compra. Queremos ser reconocidos a nivel nacional como una plataforma segura y confiable donde los clientes se sienten cómodos de navegar entre los distintos modelos de ropa que tendremos y saber que una vez cierre la compra su mercancía llegará sana y salva. 
+            <br>
+          Queremos lograr que nuestros clientes una vez hagan su compra esperen con emoción su artículo y que podamos ser nosotros los que brindemos esa felicidad a miles de personas.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="about__item">
+          <h4>Objetivos</h4>
+          <p>Nuestro objetivo principal es innovar el mercado, queremos ser los pioneros de una plataforma en línea de venta de ropa importada y que nuestro consumidores vean que la era digital puede facilitarles mucho la vida y además obtener un gran producto ya en tierra nacional sin tener que pasar malos momentos tratando de comprar en el exterior.</p>
         </div>
       </div>
     </div>
@@ -58,7 +109,7 @@
 <!-- About Section End -->
 
 <!-- Team Section Begin -->
-<section class="team spad">
+{{-- <section class="team spad">
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
@@ -99,7 +150,7 @@
       </div>
     </div>
   </div>
-</section>
+</section> --}}
 <!-- Team Section End -->
 
 <!-- Client Section Begin -->
@@ -108,22 +159,22 @@
     <div class="row">
       <div class="col-lg-12">
         <div class="section-title">
-          <span>Partner</span>
-          <h2>Happy Clients</h2>
+          {{-- <span>Partner</span> --}}
+          <h2>Marcas que disponemos</h2>
         </div>
       </div>
     </div>
     <div class="row">
-      <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-        <a href="#" class="client__item"><img src="storage/img/clients/client-1.png" alt=""></a>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-6">
+        <a href="#" class="client__item"><img src="storage/img/clients/hollister-logo-black-and-white.png" alt="" width="50%"></a>
       </div>
-      <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-        <a href="#" class="client__item"><img src="storage/img/clients/client-2.png" alt=""></a>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-6 mt-4">
+        <a href="#" class="client__item"><img src="storage/img/clients/Zara-Logo.png" alt="" width="50%"></a>
       </div>
-      <div class="col-lg-3 col-md-4 col-sm-4 col-6">
-        <a href="#" class="client__item"><img src="storage/img/clients/client-3.png" alt=""></a>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-6">
+        <a href="#" class="client__item"><img src="storage/img/clients/abercrombie&fitch.png" alt="" width="50%"></a>
       </div>
-      <div class="col-lg-3 col-md-4 col-sm-4 col-6">
+      {{-- <div class="col-lg-3 col-md-4 col-sm-4 col-6">
         <a href="#" class="client__item"><img src="storage/img/clients/client-4.png" alt=""></a>
       </div>
       <div class="col-lg-3 col-md-4 col-sm-4 col-6">
@@ -137,7 +188,7 @@
       </div>
       <div class="col-lg-3 col-md-4 col-sm-4 col-6">
         <a href="#" class="client__item"><img src="storage/img/clients/client-8.png" alt=""></a>
-      </div>
+      </div> --}}
     </div>
   </div>
 </section>
